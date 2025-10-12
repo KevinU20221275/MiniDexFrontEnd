18cca5df2f76e95f1451d884f8b83560b43e7e98
KevinU20221275 <U20221275@univo.edu.sv>
2025-10-11 18:22:35 -0600

    add configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node"
 
 import react from '@astrojs/react';
 
@@ -10,6 +11,9 @@ export default defineConfig({
   integrations: [react()],
 
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 
   vite: {
     plugins: [tailwindcss()]
