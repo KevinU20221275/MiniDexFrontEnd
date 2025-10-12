3377a52b908a344f8c8be4dcd18efbe6ecae379a
KevinU20221275 <U20221275@univo.edu.sv>
2025-10-11 18:30:54 -0600

    add register api endpoint

---
+++ src/pages/api/register.ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request,cookies }) => {
+  const body = await request.json();
+
+  const res = await fetch("http://localhost:8080/auth/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+
+  const data = await res.json();
+  
+  if (res.ok){
+    cookies.set("token", data.token, {
+        path: "/",
+        httpOnly: true,
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production"
+    })
+
+    return new Response(JSON.stringify({ok: true, trainer: data.trainerDTO}), {status: 200, headers: {"Content-Type": "application/json"}})
+  }
+
+  return new Response(JSON.stringify(data), {status: res.status})
+};
