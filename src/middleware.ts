01e021b79d10ebb9fad8bf4416b184d34c21a8f9
KevinU20221275 <U20221275@univo.edu.sv>
2025-10-11 18:28:32 -0600

    add middleware to protect urls

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import type { MiddlewareHandler } from "astro";
+
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+    const token = context.cookies.get("token")?.value
+    const pathname = context.url.pathname
+
+    // permite rutas publicas
+    if (pathname.startsWith("/login") || pathname.startsWith("/api/login") || pathname.startsWith("/api/register") ){
+        return next()
+    }
+
+    // si no existe el token protege la ruta y redirige al login
+    if (!token){
+        return Response.redirect(new URL("/login", context.url), 302)
+    }
+
+    // guardar token en locals
+    context.locals.token = token;
+    return next()
+}
