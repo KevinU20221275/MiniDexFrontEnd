e71f101b9581259376d4444fc297384f7654e942
KevinU20221275 <kevinmotanodev@gmail.com>
2026-02-09 11:39:43 -0600

    feat: add trainer update api endpoint

--- src/pages/api/trainer/update.ts
+++ src/pages/api/trainer/update.ts
@@ -1,66 +1,24 @@
+import { handleApiError } from "@/server/errors/handleApiError";
+import { updateTrainerData } from "@/server/services/trainer.service";
+import type { UpdateNameAndTrainerUsernameRequest } from "@/server/types/trainer.types";
 import type { APIRoute } from "astro";
 
-export const PATCH: APIRoute = async ({locals, request}) => {
-    const token = locals.token
- 
-    if (!token) {
-        return new Response("No autorizado", {status: 401})
-    }
-
-    const body = await request.json();
-
-    const res = await fetch("http://localhost:8080/trainers/me/coins", {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        },
-        body: JSON.stringify(body)
-    })
-
-
-    if (!res.ok) {
-        return new Response("Error al actualizar el entrenador", { status: res.status });
-    }
-
-    const updatedTrainer = await res.json();
-
-    return new Response(JSON.stringify(updatedTrainer), { status: 200 });
-}
-
-
 export const PUT:APIRoute = async({locals, request, cookies}) => {
-    const token = locals.token
+    try {
+        const body = (await request.json() as UpdateNameAndTrainerUsernameRequest)
 
-    if (!token) {
-        return new Response("No autorizado", {status: 401})
+        const data = await updateTrainerData(locals.token!, body)
+
+        // Guarda el nuevo token en la cookie
+        cookies.set("token", data.token, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "lax",
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        return Response.json(data.trainerDTO)
+    } catch (error) {
+        return handleApiError(error)
     }
-
-    const body = await request.json();
-
-    const res = await fetch("http://localhost:8080/trainers/me", {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        },
-        body: JSON.stringify(body)
-    })
-
-
-    if (!res.ok) {
-        return new Response("Error al actualizar el entrenador", { status: res.status });
-    }
-
-    const updatedTrainer = await res.json();
-
-    // Guarda el nuevo token en la cookie
-    cookies.set("token", updatedTrainer.token, {
-        path: "/",
-        httpOnly: true,
-        sameSite: "lax",
-        secure: process.env.NODE_ENV === "production"
-    })
-
-    return new Response(JSON.stringify({trainer: updatedTrainer.trainer}), { status: 200, headers: {"Content-Type" : "application/json"} });
 }
